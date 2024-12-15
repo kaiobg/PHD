c491556a8d4b0aa1d06513fb661c1831822c425a
Nathália Pissuti <nathaliapissuti@gmail.com>
2024-12-15 13:13:26 -0300

    fix: Changed script to use correct field (password)

--- login/index.js
+++ login/index.js
@@ -1,23 +1,15 @@
 import { firebaseService } from '../services';
 
-// TEMP CODE
-window.email = 'teste5@teste.com';
-window.password = 'senha123';
-window.signIn = firebaseService.auth.signIn;
-window.signOut = firebaseService.auth.signOut;
+const onSubmitForm = async (e) => {
+  e.preventDefault();
+  
+  const form = e.target;
+  const email = form.email.value;
+  const password = form.password.value;
 
+  await firebaseService.auth.signIn(email, password);
 
+  window.location = '/coach/';
+};
 
-const onSubmitForm = (e)=>{
-    e.preventDefault ()
-    console.log(e)
-    const form=e.target
-    const email=form.email.value
-    const senha=form.senha.value
-
-    firebaseService.auth.signIn(email,senha);
-
-    window.location = "/coach/";
-}
-
-document.querySelector("#identification-form").addEventListener("submit",onSubmitForm)
+document.querySelector('#identification-form').addEventListener('submit', onSubmitForm);
